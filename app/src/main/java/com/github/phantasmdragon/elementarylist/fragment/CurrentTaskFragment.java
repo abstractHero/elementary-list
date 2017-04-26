4398f586858c272eced09cc9d027e403feb14580
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-04-26 23:39:35 +0300

    Instead of ListFragment and ListView, a Fragment and a RecyclerView are used, respectively

--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CurrentTaskFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CurrentTaskFragment.java
@@ -2,7 +2,9 @@ package com.github.phantasmdragon.elementarylist.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +17,11 @@ import org.jetbrains.annotations.Contract;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class CurrentTaskFragment extends ListFragment {
+public class CurrentTaskFragment extends Fragment {
 
-    //Для демонстрации, удалится в скором времени
-    private final String[] catNames = new String[]{"Один", "Два", "Три",};
-
-    private CustomRowAdapter rowListAdapter;
+    //Array List how example for demonstration. Will be deleted
+    private ArrayList<String> tasks = new ArrayList<>(Arrays.asList("Hi, how are you?", "Hi, how are you?", "Hi, how are you?",
+            "Hi, how are you?", "Hi, how are you?", "Hi, how are you?", "Hi, how are you?", "Hi, how are you?"));
 
     @Contract(" -> !null")
     public static CurrentTaskFragment newInstance() {
@@ -30,15 +31,17 @@ public class CurrentTaskFragment extends ListFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        CustomRowAdapter rowAdapter = new CustomRowAdapter(getActivity().getApplicationContext(), tasks);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
 
-        rowListAdapter = new CustomRowAdapter(getActivity(), android.R.layout.simple_list_item_1, new ArrayList<>(Arrays.asList(catNames)));
-        setListAdapter(rowListAdapter);
+        RecyclerView currentTaskRecycler = (RecyclerView) getActivity().findViewById(R.id.recycler_current_task);
+        currentTaskRecycler.setAdapter(rowAdapter);
+        currentTaskRecycler.setLayoutManager(layoutManager);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_current_task, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_current_task, container, false);
     }
 }
