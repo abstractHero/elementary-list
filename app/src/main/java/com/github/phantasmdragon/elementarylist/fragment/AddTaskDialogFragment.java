df4271ec59f3d415cf478c0426ff163625e7b85f
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-04-28 17:57:53 +0300

    Dialog class added

---
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/AddTaskDialogFragment.java
@@ -0,0 +1,37 @@
+package com.github.phantasmdragon.elementarylist.fragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.github.phantasmdragon.elementarylist.R;
+
+public class AddTaskDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_add_task, null))
+                .setTitle(getResources().getString(R.string.dialog_title))
+                .setPositiveButton("Отмена", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("Okay", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+}
