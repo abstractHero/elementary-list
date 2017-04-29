e708b52cdb7cfb6cb22be43c2694e6347f5c96db
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-04-29 19:59:03 +0300

    Added method of information transfer from DialogFragment into MainActivity

--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/AddTaskDialogFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/AddTaskDialogFragment.java
@@ -1,16 +1,21 @@
 package com.github.phantasmdragon.elementarylist.fragment;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.support.annotation.NonNull;
+import android.content.DialogInterface;
 
 import com.github.phantasmdragon.elementarylist.R;
+import com.github.phantasmdragon.elementarylist.fragment.listener.AddTaskDialogListener;
 
 public class AddTaskDialogFragment extends DialogFragment {
+
+    private AddTaskDialogListener mListener;
 
     @NonNull
     @Override
@@ -20,18 +25,40 @@ public class AddTaskDialogFragment extends DialogFragment {
 
         builder.setView(inflater.inflate(R.layout.dialog_add_task, null))
                 .setTitle(getResources().getString(R.string.dialog_title))
-                .setPositiveButton("Отмена", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        // sign in the user ...
+                        sendInfoFromDialog();
                     }
                 })
-                .setNegativeButton("Okay", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
                     }
                 });
 
         return builder.create();
+    }
+
+    private void sendInfoFromDialog() {
+        EditText enteredName = (EditText)getDialog().findViewById(R.id.edit_task_name);
+        String taskName = "";
+        if (enteredName != null) taskName = enteredName.getText().toString();
+
+        Bundle info = new Bundle();
+        info.putString("task_name", taskName);
+
+        mListener.onDialogPositiveClick(this, info);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mListener = (AddTaskDialogListener)getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString() + " must implement AddTaskDialogListener");
+        }
     }
 }
