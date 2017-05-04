ef7b2f7d38226c50d96f4e9d023a93d3af6bb051
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-04 22:55:28 +0300

    Was renamed. Now extends from the TemplateTaskFragment and passes needed data at initialization

--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/UnfulfilledTaskFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/UnfulfilledTaskFragment.java
@@ -4,16 +4,16 @@ import android.os.Bundle;
 
 import com.github.phantasmdragon.elementarylist.fragment.template.TemplateTaskFragment;
 
-public class CurrentTaskFragment extends TemplateTaskFragment {
+public class UnfulfilledTaskFragment extends TemplateTaskFragment {
 
     private static final String NAME_LIST_ID = "unfulfilledId";
     private static final String NAME_LIST = "unfulfilledList";
-    private static final String NAME_FILE = "CurrentTaskList";
+    private static final String NAME_FILE = "UnfulfilledTaskList";
 
-    public static CurrentTaskFragment newInstance() {
-        CurrentTaskFragment currentTaskFragment = new CurrentTaskFragment();
-        currentTaskFragment.setArguments(getNames());
-        return currentTaskFragment;
+    public static UnfulfilledTaskFragment newInstance() {
+        UnfulfilledTaskFragment unfulfilledTaskFragment = new UnfulfilledTaskFragment();
+        unfulfilledTaskFragment.setArguments(getNames());
+        return unfulfilledTaskFragment;
     }
 
     private static Bundle getNames() {
@@ -21,7 +21,7 @@ public class CurrentTaskFragment extends TemplateTaskFragment {
         names.putString("nameList", NAME_LIST);
         names.putString("nameFile", NAME_FILE);
         names.putString("nameListId", NAME_LIST_ID);
-        names.putString("nameFragment", CurrentTaskFragment.class.getSimpleName());
+        names.putString("nameFragment", UnfulfilledTaskFragment.class.getSimpleName());
         return names;
     }
 }
