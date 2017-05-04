8fa7d138f7738e60cb401234b44ad1a3cf22994e
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-04 23:00:10 +0300

    Now extends from the TemplateTaskFragment and passes necessary data when initialized

--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CompletedTaskFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CompletedTaskFragment.java
@@ -1,162 +1,27 @@
 package com.github.phantasmdragon.elementarylist.fragment;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.github.phantasmdragon.elementarylist.R;
-import com.github.phantasmdragon.elementarylist.activity.MainActivity;
-import com.github.phantasmdragon.elementarylist.custom.rowadapter.CustomRowAdapter;
+import com.github.phantasmdragon.elementarylist.fragment.template.TemplateTaskFragment;
 
-import org.jetbrains.annotations.Contract;
+public class CompletedTaskFragment extends TemplateTaskFragment {
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
+    private static final String NAME_LIST_ID = "completedId";
+    private static final String NAME_LIST = "completedList";
+    private static final String NAME_FILE = "CompletedTaskList";
 
-public class CompletedTaskFragment extends Fragment {
-
-    private final String NAME_LIST_ID = "completedId";
-    private final String NAME_LIST = "completedList";
-    private final String NAME_FILE = "CompletedTaskList";
-
-    private CustomRowAdapter rowAdapter;
-    private RecyclerView completedTaskRecycler;
-    private SharedPreferences completedTaskPreference;
-
-    private ArrayList<String> completedTasks = new ArrayList<>();
-    private ArrayList<String> completedTasksId = new ArrayList<>();
-
-    @Contract(" -> !null")
     public static CompletedTaskFragment newInstance() {
-        return new CompletedTaskFragment();
+        CompletedTaskFragment completedTaskFragment = new CompletedTaskFragment();
+        completedTaskFragment.setArguments(getNames());
+        return completedTaskFragment;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        completedTaskPreference = getActivity().getSharedPreferences(NAME_FILE, Context.MODE_PRIVATE);
-        loadTask();
-        setRetainInstance(true);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_completed_task, container, false);
-
-        rowAdapter = new CustomRowAdapter(view.getContext(), completedTasks, CompletedTaskFragment.class.getSimpleName());
-
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        if(savedInstanceState != null) {
-            completedTasks = savedInstanceState.getStringArrayList(NAME_LIST);
-            completedTasksId = savedInstanceState.getStringArrayList(NAME_LIST_ID);
-        }
-
-        completedTaskRecycler = (RecyclerView)getActivity().findViewById(R.id.recycler_completed_task);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        completedTaskRecycler.setLayoutManager(layoutManager);
-        completedTaskRecycler.setAdapter(rowAdapter);
-
-        final FloatingActionButton fab = ((FloatingActionButton)getActivity().findViewById(R.id.button_float));
-        completedTaskRecycler.addOnScrollListener(new RecyclerView.OnScrollListener(){
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                if (dy > 0) fab.hide();
-                else        fab.show();
-            }
-        });
-    }
-
-    private void loadTask() {
-        Set<String> keySet = completedTaskPreference.getAll().keySet();
-        TreeSet<String> sortKeySet = new TreeSet<>(keySet);
-        for (String key: sortKeySet) {
-            completedTasks.add(completedTaskPreference.getString(key, ""));
-            completedTasksId.add(key.substring(key.indexOf("_")+1));
-        }
-    }
-
-    private void saveTask(String taskId, Bundle taskInfo) {
-        SharedPreferences.Editor saveEditor = completedTaskPreference.edit();
-        saveEditor.putString(taskId, taskInfo.getString(MainActivity.NAME_TASK))
-                .apply();
-    }
-
-    private void deleteTask(String taskId) {
-        SharedPreferences.Editor deleteEditor = completedTaskPreference.edit();
-        if (completedTaskPreference.contains(taskId)) deleteEditor.remove(taskId).apply();
-        else deleteEditor.remove(getKeyWithoutPrefix(taskId)).apply();
-    }
-
-    public Bundle getInfoAboutTask(int position) {
-        Bundle infoAboutTask = new Bundle();
-        infoAboutTask.putString(MainActivity.NAME_TASK, completedTasks.get(position));
-        return infoAboutTask;
-    }
-
-    public void addTask(Bundle infoAboutNewTask) {
-        completedTasksId.add(0, new UUID(System.currentTimeMillis(), System.nanoTime()).toString());
-
-        completedTasks.add(0, infoAboutNewTask.getString(MainActivity.NAME_TASK));
-        saveTask(completedTasksId.get(0), infoAboutNewTask);
-        rowAdapter.notifyItemRangeChanged(0, completedTasks.size());
-
-        completedTaskRecycler.scrollToPosition(0);
-    }
-
-    public void removeTask(int position) {
-        deleteTask(getKey(position, completedTasksId.get(position)));
-        completedTasks.remove(position);
-        completedTasksId.remove(position);
-        rowAdapter.notifyItemRemoved(position);
-        rowAdapter.notifyItemRangeChanged(0, completedTasks.size());
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putStringArrayList(NAME_LIST, completedTasks);
-        outState.putStringArrayList(NAME_LIST_ID, completedTasksId);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        SharedPreferences.Editor positionEditor = completedTaskPreference.edit();
-        positionEditor.clear();
-        for (int i = 0; i < completedTasks.size(); i++) {
-
-            String id = completedTasksId.get(i),
-                    key = getKey(i, id);
-
-            positionEditor.putString(key, completedTasks.get(i));
-        }
-        positionEditor.apply();
-    }
-
-    @NonNull
-    private String getKey(int index, String key) {
-        return String.valueOf(index).concat("_" + key);
-    }
-
-    @NonNull
-    private String getKeyWithoutPrefix(String key) {
-        return key.substring(key.indexOf("_")+1);
+    private static Bundle getNames() {
+        Bundle names = new Bundle();
+        names.putString("nameList", NAME_LIST);
+        names.putString("nameFile", NAME_FILE);
+        names.putString("nameListId", NAME_LIST_ID);
+        names.putString("nameFragment", CompletedTaskFragment.class.getSimpleName());
+        return names;
     }
 }
--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/SpecialTaskFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/SpecialTaskFragment.java
@@ -1,162 +1,27 @@
 package com.github.phantasmdragon.elementarylist.fragment;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.github.phantasmdragon.elementarylist.R;
-import com.github.phantasmdragon.elementarylist.activity.MainActivity;
-import com.github.phantasmdragon.elementarylist.custom.rowadapter.CustomRowAdapter;
+import com.github.phantasmdragon.elementarylist.fragment.template.TemplateTaskFragment;
 
-import org.jetbrains.annotations.Contract;
+public class SpecialTaskFragment extends TemplateTaskFragment {
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
+    private static final String NAME_LIST_ID = "specialId";
+    private static final String NAME_LIST = "specialList";
+    private static final String NAME_FILE = "SpecialTaskList";
 
-public class SpecialTaskFragment extends Fragment {
-
-    private final String NAME_LIST_ID = "specialId";
-    private final String NAME_LIST = "specialList";
-    private final String NAME_FILE = "SpecialTaskList";
-
-    private CustomRowAdapter rowAdapter;
-    private RecyclerView specialTaskRecycler;
-    private SharedPreferences specialTaskPreference;
-
-    private ArrayList<String> specialTasks = new ArrayList<>();
-    private ArrayList<String> specialTasksId = new ArrayList<>();
-
-    @Contract(" -> !null")
     public static SpecialTaskFragment newInstance() {
-        return new SpecialTaskFragment();
+        SpecialTaskFragment specialTaskFragment = new SpecialTaskFragment();
+        specialTaskFragment.setArguments(getNames());
+        return specialTaskFragment;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        specialTaskPreference = getActivity().getSharedPreferences(NAME_FILE, Context.MODE_PRIVATE);
-        loadTask();
-        setRetainInstance(true);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_special_task, container, false);
-
-        rowAdapter = new CustomRowAdapter(view.getContext(), specialTasks, SpecialTaskFragment.class.getSimpleName());
-
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        if(savedInstanceState != null) {
-            specialTasks = savedInstanceState.getStringArrayList(NAME_LIST);
-            specialTasksId = savedInstanceState.getStringArrayList(NAME_LIST_ID);
-        }
-
-        specialTaskRecycler = (RecyclerView)getActivity().findViewById(R.id.recycler_special_task);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        specialTaskRecycler.setLayoutManager(layoutManager);
-        specialTaskRecycler.setAdapter(rowAdapter);
-
-        final FloatingActionButton fab = ((FloatingActionButton)getActivity().findViewById(R.id.button_float));
-        specialTaskRecycler.addOnScrollListener(new RecyclerView.OnScrollListener(){
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                if (dy > 0) fab.hide();
-                else        fab.show();
-            }
-        });
-    }
-
-    private void loadTask() {
-        Set<String> keySet = specialTaskPreference.getAll().keySet();
-        TreeSet<String> sortKeySet = new TreeSet<>(keySet);
-        for (String key: sortKeySet) {
-            specialTasks.add(specialTaskPreference.getString(key, ""));
-            specialTasksId.add(key.substring(key.indexOf("_")+1));
-        }
-    }
-
-    private void saveTask(String taskId, Bundle taskInfo) {
-        SharedPreferences.Editor saveEditor = specialTaskPreference.edit();
-        saveEditor.putString(taskId, taskInfo.getString(MainActivity.NAME_TASK))
-                .apply();
-    }
-
-    private void deleteTask(String taskId) {
-        SharedPreferences.Editor deleteEditor = specialTaskPreference.edit();
-        if (specialTaskPreference.contains(taskId)) deleteEditor.remove(taskId).apply();
-        else deleteEditor.remove(getKeyWithoutPrefix(taskId)).apply();
-    }
-
-    public void addTask(Bundle infoAboutNewTask) {
-        specialTasksId.add(0, new UUID(System.currentTimeMillis(), System.nanoTime()).toString());
-
-        specialTasks.add(0, infoAboutNewTask.getString(MainActivity.NAME_TASK));
-        saveTask(specialTasksId.get(0), infoAboutNewTask);
-        rowAdapter.notifyItemRangeChanged(0, specialTasks.size());
-
-        specialTaskRecycler.scrollToPosition(0);
-    }
-
-    public Bundle getInfoAboutTask(int position) {
-        Bundle infoAboutTask = new Bundle();
-        infoAboutTask.putString(MainActivity.NAME_TASK, specialTasks.get(position));
-        return infoAboutTask;
-    }
-
-    public void removeTask(int position) {
-        deleteTask(getKey(position, specialTasksId.get(position)));
-        specialTasks.remove(position);
-        specialTasksId.remove(position);
-        rowAdapter.notifyItemRemoved(position);
-        rowAdapter.notifyItemRangeChanged(0, specialTasks.size());
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putStringArrayList(NAME_LIST, specialTasks);
-        outState.putStringArrayList(NAME_LIST_ID, specialTasksId);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        SharedPreferences.Editor positionEditor = specialTaskPreference.edit();
-        positionEditor.clear();
-        for (int i = 0; i < specialTasks.size(); i++) {
-
-            String id = specialTasksId.get(i),
-                    key = getKey(i, id);
-
-            positionEditor.putString(key, specialTasks.get(i));
-        }
-        positionEditor.apply();
-    }
-
-    @NonNull
-    private String getKey(int index, String key) {
-        return String.valueOf(index).concat("_" + key);
-    }
-
-    @NonNull
-    private String getKeyWithoutPrefix(String key) {
-        return key.substring(key.indexOf("_")+1);
+    private static Bundle getNames() {
+        Bundle names = new Bundle();
+        names.putString("nameList", NAME_LIST);
+        names.putString("nameFile", NAME_FILE);
+        names.putString("nameListId", NAME_LIST_ID);
+        names.putString("nameFragment", SpecialTaskFragment.class.getSimpleName());
+        return names;
     }
 }
