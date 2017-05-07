014ca16847cd1fa0ac585d8dbca3975aa68db6c2
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-08 00:08:14 +0300

    Describes the algorithm for drawing line on top of each child in RecyclerView

---
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/custom/itemdecoration/CrossedOutItemDecoration.java
@@ -0,0 +1,45 @@
+package com.github.phantasmdragon.elementarylist.custom.itemdecoration;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.github.phantasmdragon.elementarylist.R;
+
+public class CrossedOutItemDecoration extends RecyclerView.ItemDecoration {
+
+    private Drawable mCrossedOut;
+
+    public CrossedOutItemDecoration(Context context) {
+        mCrossedOut = ContextCompat.getDrawable(context, R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        View child = parent.getChildAt(0);
+        RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+        int left = params.leftMargin;
+        int right = parent.getWidth() - params.rightMargin;
+        int beg = child.getBottom() + params.bottomMargin;
+        int bottom = beg + mCrossedOut.getIntrinsicHeight();
+        mCrossedOut.setBounds(left, beg/2, right, bottom/2+2);
+        mCrossedOut.draw(c);
+        beg /= 2;
+
+        for (int i = 1; i < parent.getChildCount(); i++) {
+            child = parent.getChildAt(i);
+
+            params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            bottom = top + mCrossedOut.getIntrinsicHeight();
+
+            mCrossedOut.setBounds(left, top-beg, right, bottom-beg);
+            mCrossedOut.draw(c);
+        }
+    }
+}
