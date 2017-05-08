9eaafa7ee80a6bf5b2ebf281e8fb2caf2457c2f2
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-08 19:32:30 +0300

    Validation for null is added. Previously thrown NullPointerException if list of completed tasks be is empty

--- app/src/main/java/com/github/phantasmdragon/elementarylist/custom/itemdecoration/CrossedOutItemDecoration.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/custom/itemdecoration/CrossedOutItemDecoration.java
@@ -20,26 +20,27 @@ public class CrossedOutItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
         View child = parent.getChildAt(0);
-        RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
-
-        int left = params.leftMargin;
-        int right = parent.getWidth() - params.rightMargin;
-        int beg = child.getBottom() + params.bottomMargin;
-        int bottom = beg + mCrossedOut.getIntrinsicHeight();
-        mCrossedOut.setBounds(left, beg/2, right, bottom/2+2);
-        mCrossedOut.draw(c);
-        beg /= 2;
-
-        for (int i = 1; i < parent.getChildCount(); i++) {
-            child = parent.getChildAt(i);
-
-            params = (RecyclerView.LayoutParams) child.getLayoutParams();
-
-            int top = child.getBottom() + params.bottomMargin;
-            bottom = top + mCrossedOut.getIntrinsicHeight();
-
-            mCrossedOut.setBounds(left, top-beg, right, bottom-beg);
+        if (child != null) {
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            int left = params.leftMargin;
+            int right = parent.getWidth() - params.rightMargin;
+            int beg = child.getBottom() + params.bottomMargin;
+            int bottom = beg + mCrossedOut.getIntrinsicHeight();
+            mCrossedOut.setBounds(left, beg/2, right, bottom/2+2);
             mCrossedOut.draw(c);
+            beg /= 2;
+
+            for (int i = 1; i < parent.getChildCount(); i++) {
+                child = parent.getChildAt(i);
+
+                params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                bottom = top + mCrossedOut.getIntrinsicHeight();
+
+                mCrossedOut.setBounds(left, top-beg, right, bottom-beg);
+                mCrossedOut.draw(c);
+            }
         }
     }
 }
