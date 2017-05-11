4eb11e0e64b1360353405e409bba986332ffd8b3
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-11 18:39:59 +0300

    Now it implements the ItemTouchHelperAdapter, changes the elements in the collection, and notifies the adapter

--- app/src/main/java/com/github/phantasmdragon/elementarylist/custom/rowadapter/CustomRowAdapter.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/custom/rowadapter/CustomRowAdapter.java
@@ -10,15 +10,17 @@ import android.widget.TextView;
 
 import com.github.phantasmdragon.elementarylist.R;
 import com.github.phantasmdragon.elementarylist.fragment.CompletedTaskFragment;
-import com.github.phantasmdragon.elementarylist.fragment.UnfulfilledTaskFragment;
 import com.github.phantasmdragon.elementarylist.fragment.SpecialTaskFragment;
+import com.github.phantasmdragon.elementarylist.fragment.UnfulfilledTaskFragment;
+import com.github.phantasmdragon.elementarylist.fragment.helper.ItemTouchHelperAdapter;
 import com.github.phantasmdragon.elementarylist.fragment.listener.OnCompletedClickListener;
 import com.github.phantasmdragon.elementarylist.fragment.listener.OnSpecialClickListener;
 import com.sackcentury.shinebuttonlib.ShineButton;
 
+import java.util.Collections;
 import java.util.List;
 
-public class CustomRowAdapter extends RecyclerView.Adapter<CustomRowAdapter.ViewHolder> {
+public class CustomRowAdapter extends RecyclerView.Adapter<CustomRowAdapter.ViewHolder> implements ItemTouchHelperAdapter {
 
     private final int CLICK_DELAY = 800;
 
@@ -29,10 +31,24 @@ public class CustomRowAdapter extends RecyclerView.Adapter<CustomRowAdapter.View
     private Context mContext;
     private String mNameFragment;
 
-    //boolean variable will be deleted later
     private boolean mIsOneCom = true, mIsOneImp = true;
     private long mTimeLastClickOnComplete = System.currentTimeMillis();
     private long mTimeLastClickOnImportant = System.currentTimeMillis();
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(mFragmentTasks, i, i+1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(mFragmentTasks, i, i-1);
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+        return true;
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -74,15 +90,15 @@ public class CustomRowAdapter extends RecyclerView.Adapter<CustomRowAdapter.View
     public void onBindViewHolder(CustomRowAdapter.ViewHolder holder, final int position) {
         String nameTask = mFragmentTasks.get(position);
 
-        if (mNameFragment.equals(CompletedTaskFragment.class.getSimpleName())) {
+        if (mNameFragment.equals(CompletedTaskFragment.NAME_THIS)) {
             holder.mIsTaskCompleted.setChecked(true);
             holder.mIsImportantTask.setEnabled(false);
         }
-        if (mNameFragment.equals(UnfulfilledTaskFragment.class.getSimpleName())) {
+        if (mNameFragment.equals(UnfulfilledTaskFragment.NAME_THIS)) {
             holder.mIsTaskCompleted.setChecked(false);
             holder.mIsImportantTask.setChecked(false);
         }
-        if (mNameFragment.equals(SpecialTaskFragment.class.getSimpleName())) {
+        if (mNameFragment.equals(SpecialTaskFragment.NAME_THIS)) {
             holder.mIsTaskCompleted.setChecked(false);
             holder.mIsImportantTask.setChecked(true);
         }
