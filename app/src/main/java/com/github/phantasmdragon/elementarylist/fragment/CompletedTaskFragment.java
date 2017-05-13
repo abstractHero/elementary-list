ab6d9eb5102f971eb0dca29e82a5df7a61c9b47d
Alexander CHernyaev <this.phantasmdragon@gmail.com>
2017-05-13 16:49:48 +0300

    A method is added to check, whether task is special. When adding to the task name, postfix is added: 0 - normal task, 1 - special task. In the getInfoAboutTask method, the task name is taken without postfix

--- app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CompletedTaskFragment.java
+++ app/src/main/java/com/github/phantasmdragon/elementarylist/fragment/CompletedTaskFragment.java
@@ -3,6 +3,7 @@ package com.github.phantasmdragon.elementarylist.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.github.phantasmdragon.elementarylist.activity.MainActivity;
 import com.github.phantasmdragon.elementarylist.fragment.template.TemplateTaskFragment;
 
 public class CompletedTaskFragment extends TemplateTaskFragment {
@@ -32,5 +33,34 @@ public class CompletedTaskFragment extends TemplateTaskFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setDecoration();
+    }
+
+    @Override
+    public void addTask(Bundle infoAboutNewTask) {
+        String nameTask = infoAboutNewTask.getString(MainActivity.NAME_TASK);
+        if (nameTask == null) throw new NullPointerException(CompletedTaskFragment.NAME_THIS + ": nameTask is null in addTask");
+
+        if (infoAboutNewTask.containsKey(SpecialTaskFragment.IS_SPECIAL)) {
+            infoAboutNewTask.putString(MainActivity.NAME_TASK, nameTask.concat("1"));
+        } else {
+            infoAboutNewTask.putString(MainActivity.NAME_TASK, nameTask.concat("0"));
+        }
+        super.addTask(infoAboutNewTask);
+    }
+
+    @Override
+    public Bundle getInfoAboutTask(int position) {
+        Bundle info = super.getInfoAboutTask(position);
+        String nameTask = info.getString(MainActivity.NAME_TASK);
+        if (nameTask == null) throw new NullPointerException(CompletedTaskFragment.NAME_THIS + ": nameTask is null in getInfoAboutTask");
+
+        info.putString(MainActivity.NAME_TASK, nameTask.substring(0, nameTask.length()-1));
+        return info;
+    }
+
+    public boolean isSpecialTask(int position) {
+        Bundle info = super.getInfoAboutTask(position);
+        String nameTask = info.getString(MainActivity.NAME_TASK);
+        return nameTask != null && nameTask.endsWith("1");
     }
 }
